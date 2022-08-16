5a2f1bbd3502bc5c80db94b436d4aec33232abd6
Nicolas <nicolasariel.enriquez@nttdata.com>
2022-08-16 15:10:07 -0300

    Desafio-4

---
+++ Desafio4-ApiRestFul/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const PORT = 8080;
+
+const server = app.listen(PORT, () => {
+    console.log('Servidor Iniciado');
+});
+
+server.on('error', err => console.log('Error en el servidor', err));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(require('./routes'));
